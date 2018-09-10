e8d1b7f5f24725a2994d9ed283d57c7d2df38ba2
Rodrigo Saito <rodrigo.ksaito@gmail.com>
2018-09-09 23:18:51 -0300

    exemplo_es_01.py
    Commit inicial

---
+++ LaboratorioDeAlgoritmos/Aula03B/exemplo_es_01.py
@@ -0,0 +1,9 @@
+print("Programa que calcula a idade de uma pessoa\n\n")
+
+ano_atual = int(input("Informa o ano atual: "))
+
+ano_nasc = int(input("Informa o ano de nascimento: "))
+
+idade = ano_atual - ano_nasc
+
+print("Voce tem %d anos." % (idade))
