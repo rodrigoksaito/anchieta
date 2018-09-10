7b88007b380b7ad407fb4edf4a9510eebff1adee
Rodrigo Saito <rodrigo.ksaito@gmail.com>
2018-09-10 02:08:09 -0300

    Exmplos de in e is em python
    Commit inicial

---
+++ LaboratorioDeAlgoritmos/Aula05A/exemplo_in_notin.py
@@ -0,0 +1,18 @@
+'''
+Utilizando in e not in
+'''
+
+def main():
+    frase = "Olá mundo!"
+    dicionario = {1:'chave',2:'valor'}
+   
+    print('O' in frase)
+
+    print('Ola' not in frase)
+
+    print(1 in dicionario)
+    
+    print('palavra' in dicionario)
+        
+if __name__ == '__main__':
+    main()
---
+++ LaboratorioDeAlgoritmos/Aula05A/exemplo_is_notis.py
@@ -0,0 +1,21 @@
+'''
+Utilizando is e not is
+'''
+
+def main():
+    valor1 = 10
+    valor2 = 10
+    string1 = 'Teste'
+    string2 = 'Teste'
+    lista1 = [10,20,30]
+    lista2 = [10,20,30]
+    
+    print(valor1 is not valor2)
+
+    print(string1 is string2)
+
+    print(lista1 is lista2)
+    
+        
+if __name__ == '__main__':
+    main()
