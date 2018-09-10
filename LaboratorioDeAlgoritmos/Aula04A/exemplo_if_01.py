ced2f5c55ad9aad654786bd6a642dd4c4b35ec63
Rodrigo Saito <rodrigo.ksaito@gmail.com>
2018-09-09 23:20:04 -0300

    exemplo_if_01.py
    Commit inicial

---
+++ LaboratorioDeAlgoritmos/Aula04A/exemplo_if_01.py
@@ -0,0 +1,10 @@
+def main():
+    opcao = int(input("Deseja Continuar? Digite 1 para SIM e 2 para NAO "))
+
+    if opcao == 1:
+        print("Usuario escolheu continuar")
+
+if __name__ == '__main__':
+    main()
+
+
